1f71fcedf19ca1eab6facda4a50bd9d9b74bbfdc
nguyentcnnl361511 <nguyentcnnl361511@gmail.com>
2026-03-02 14:39:33 +0700

    no message

---
+++ BAITAPTUAN4/MyGeneriClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace BAITAPTUAN4
+{
+    public class MyGeneriClass<T> where T : IComparable<T>
+    {
+        // Phương thức in mảng
+        public void PrintArray(T[] inputArray)
+        {
+            foreach (T element in inputArray)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+
+        // Phương thức sắp xếp (Bubble Sort)
+        public T[] Sort(T[] inputArray)
+        {
+            int n = inputArray.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (inputArray[j].CompareTo(inputArray[j + 1]) > 0)
+                    {
+                        T temp = inputArray[j];
+                        inputArray[j] = inputArray[j + 1];
+                        inputArray[j + 1] = temp;
+                    }
+                }
+            }
+
+            return inputArray;
+        }
+    }
+}
---
+++ BAITAPTUAN4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace BAITAPTUAN4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            MyGeneriClass<int> intObj = new MyGeneriClass<int>();
+            int[] arrInt = { 5, 2, 9, 1 };
+
+            Console.WriteLine("Mảng int trước khi sắp xếp:");
+            intObj.PrintArray(arrInt);
+
+            intObj.Sort(arrInt);
+
+            Console.WriteLine("Mảng int sau khi sắp xếp:");
+            intObj.PrintArray(arrInt);
+
+            Console.ReadKey();
+        }
+    }
+}
